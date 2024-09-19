d37aa76a2480db17bab19d16cc55688043851297
Zareen100 <monzu001976@gmail.com>
2024-09-19 20:25:05 +0600

    project for 29th june

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Welcome to the world of programming!")
+#Print a number
+print(4)
+#Next line after print
+print("hello python \n")
+#print all the keywords present in the python
+print("keyboards in the python are...")
+import keyword
+print(keyword.kwlist)
